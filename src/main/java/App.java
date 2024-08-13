5a81ec02ee99b17cd000980bc26ccbbda4714e6a
ViktoriaVolchek <167359085+ViktoriaVolchek@users.noreply.github.com>
2024-08-13 14:00:42 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import Cat1.Cat;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,5 +9,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(catBean1 == catBean2);
     }
 }
